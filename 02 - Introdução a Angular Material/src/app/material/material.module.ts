290e3fd4c13d6d06b7022fb930afd6c985c9b0d9
Portfolio de Projetos e Cursos de Tecnologia e Engenharia <waldirweber@gmail.com>
2024-06-27 02:01:53 -0300

    criando o Material Module

---
+++ 02 - Introdução a Angular Material/src/app/material/material.module.ts
@@ -0,0 +1,10 @@
+import { MatSliderModule} from '@angular/material/slider'
+@NgModule({
+  declarations: [],
+  imports: [
+    MatSliderModule
+  ],
+  exports: [
+  ]
+})
+export class MaterialModule { }

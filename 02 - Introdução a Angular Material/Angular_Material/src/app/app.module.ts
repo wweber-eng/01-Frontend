00213848f2ba7311884064fdd78079b50967494a
Portfolio de Projetos e Cursos de Tecnologia e Engenharia <waldirweber@gmail.com>
2024-06-28 01:03:56 -0300

    Criação do projeto

--- 02 - Introdução a Angular Material/Angular_Material/src/app/app.module.ts
+++ 02 - Introdução a Angular Material/Angular_Material/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MaterialModule } from './material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +12,7 @@ import { MaterialModule } from './material/material.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MaterialModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

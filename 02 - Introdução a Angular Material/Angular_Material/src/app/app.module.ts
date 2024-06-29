9f959727893a21dd5bbf32fd4804e4675d5fbcc3
Portfolio de Projetos e Cursos de Tecnologia e Engenharia <waldirweber@gmail.com>
2024-06-29 18:47:04 -0300

    Implementando o Icon Button

--- 02 - Introdução a Angular Material/Angular_Material/src/app/app.module.ts
+++ 02 - Introdução a Angular Material/Angular_Material/src/app/app.module.ts
@@ -5,10 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { ButtonsIconComponent } from './buttons-icon/buttons-icon.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonsIconComponent,
   ],
   imports: [
     BrowserModule,

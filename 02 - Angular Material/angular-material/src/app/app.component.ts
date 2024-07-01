1c5b976a457127669a685d9f6d5a798550d40a5a
Portfolio de Projetos e Cursos de Tecnologia e Engenharia <waldirweber@gmail.com>
2024-06-30 22:56:07 -0300

    04 - Finalizando Sidenav Dinamico

--- 02 - Angular Material/angular-material/src/app/app.component.ts
+++ 02 - Angular Material/angular-material/src/app/app.component.ts
@@ -1,26 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver } from '@angular/cdk/layout';
+import { fromEvent, map } from 'rxjs';
 
-
+export const SCROLL_CONTAINER = 'mat-sidenav-content';
+export const TEXT_LIMIT = 50;
+export const SHADOW_LIMIT = 100;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-
-  constructor(private breakpointObserver: BreakpointObserver){}
+  export class AppComponent implements OnInit  {
 
     public isSmallScreen = false;
+    public popText = false;
+    public applyShadow = false;
+
+    constructor(private breakpointObserver: BreakpointObserver){ }
+
+    ngOnInit(): void {
+      const content = document.getElementsByClassName(SCROLL_CONTAINER)[0];
+
+      fromEvent( content, 'scroll')
+      .pipe(
+        map(() => content.scrollTop)
+      )
+      .subscribe(
+        (value: number) => this.determineHeader(value)
+      )
+    }
 
     ngAfterContentInit(): void {
       this.breakpointObserver
-      .observe(['(max-width: 800px)'])
-      .subscribe((res) => this.isSmallScreen = res.matches)
+          .observe(['(max-width: 800px)'])
+          .subscribe((res) => this.isSmallScreen = res.matches);
     }
 
+    determineHeader(top: number){
+        this.popText = top >= TEXT_LIMIT;
+        this.applyShadow = top >= SHADOW_LIMIT;
+    }
 
     get SidenavMode(){
-      return this.isSmallScreen ? 'over' : 'side';
+      return this.isSmallScreen ? 'over' : 'push';
     }
-}
+
+  }
+
+

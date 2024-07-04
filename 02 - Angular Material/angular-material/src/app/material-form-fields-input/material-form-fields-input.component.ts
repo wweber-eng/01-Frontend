138f3da6bef6f655b11e7cca59f96c4451b731d1
Portfolio de Projetos e Cursos de Tecnologia e Engenharia <waldirweber@gmail.com>
2024-07-03 22:23:57 -0300

    06 - Adicionando FormFiled e Inputs. Adicionando Rotas

---
+++ 02 - Angular Material/angular-material/src/app/material-form-fields-input/material-form-fields-input.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-material-form-fields-input',
+  templateUrl: './material-form-fields-input.component.html',
+  styleUrls: ['./material-form-fields-input.component.scss']
+})
+export class MaterialFormFieldsInputComponent {
+ public value = '';
+}

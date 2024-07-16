121ad57f88bd62b102f9f091176e52960ebb4c1b
Portfolio de Projetos e Cursos de Tecnologia e Engenharia <waldirweber@gmail.com>
2024-07-16 19:10:21 -0300

    101 - Criando o Componente Title

--- 03 - Angular Material Avançado/ProjectSchool/src/app/app.module.ts
+++ 03 - Angular Material Avançado/ProjectSchool/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModuleModule } from './shared/materials/material.module';
+import { UsersMatModuleModule } from './shared/materials/users-mat.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModuleModule,
+    UsersMatModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ 03 - Angular Material Avançado/ProjectSchool/src/app/pages/users/my-conquests/my-conquests.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-conquests',
+  templateUrl: './my-conquests.component.html',
+  styleUrls: ['./my-conquests.component.scss']
+})
+export class MyConquestsComponent {
+
+}
